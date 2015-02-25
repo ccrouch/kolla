ff7d1d87395c2b81a10d34922120ec164b25702c
ccrouch <charles.crouch@gmail.com>
2015-02-24 22:31:37 -0600

    fix missing db connection setting in ceil.conf

--- docker/ceilometer/ceilometer-collector/start.sh
+++ docker/ceilometer/ceilometer-collector/start.sh
@@ -5,5 +5,11 @@ set -e
 . /opt/kolla/kolla-common.sh
 . /opt/kolla/config-ceilometer.sh
 
+cfg=/etc/ceilometer/ceilometer.conf
+crudini --set $cfg \
+    database connection \
+    "mysql://${CEILOMETER_DB_USER}:${CEILOMETER_DB_PASSWORD}@${MARIADB_SERVICE_HOST}/${CEILOMETER_DB_NAME}"
+
+
 
 exec /usr/bin/ceilometer-collector
