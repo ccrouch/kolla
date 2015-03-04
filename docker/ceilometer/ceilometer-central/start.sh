f9f3153f934ba7e56945530e199d97df1c1dc798
ccrouch <charles.crouch@gmail.com>
2015-03-04 03:24:08 +0000

    wait for keystone properly, stop checking db

--- docker/ceilometer/ceilometer-central/start.sh
+++ docker/ceilometer/ceilometer-central/start.sh
@@ -5,8 +5,14 @@
 
 check_required_vars KEYSTONE_AUTH_PROTOCOL CEILOMETER_KEYSTONE_USER CEILOMETER_ADMIN_PASSWORD ADMIN_TENANT_NAME
 
+# this should return successfully only after the ceilometer keystone user
+# has been properly setup
+/opt/kolla/wait_for 30 1 keystone \
+                    --os-auth-url=http://${KEYSTONE_PUBLIC_SERVICE_HOST}:${KEYSTONE_PUBLIC_SERVICE_PORT}/v2.0 \
+                    --os-username=${CEILOMETER_KEYSTONE_USER} \
+                    --os-tenant-name=${ADMIN_TENANT_NAME} \
+                    --os-password=${CEILOMETER_ADMIN_PASSWORD} token-get
 check_for_keystone
-check_for_db
 
 export SERVICE_TOKEN="${KEYSTONE_ADMIN_TOKEN}"
 export SERVICE_ENDPOINT="${KEYSTONE_AUTH_PROTOCOL}://${KEYSTONE_ADMIN_SERVICE_HOST}:${KEYSTONE_ADMIN_SERVICE_PORT}/v2.0"
